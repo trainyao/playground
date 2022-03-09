f0bf7953c2528b10dca246591dd3dbfcdf3d03f2
trainyao <trainyao@gmail.com>
2022-03-09 09:14:16 +0800

    kb

---
+++ k8s/crd-controller/kb/api/v1/goddessmoment_types.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//GoddessMomentSpec .
+type GoddessMomentSpec struct {
+	FoodDemand []FoodDemand `json:"foodDemand,omitempty"`
+}
+
+//FoodDemand .
+type FoodDemand struct {
+	Name string `json:"name,omitempty"`
+}
+
+//GoddessMomentStatus .
+type GoddessMomentStatus struct {
+	FoodDemand []FoodDemandStatus `json:"foodDemand,omitempty"`
+}
+
+//FoodDemandStatus .
+type FoodDemandStatus struct {
+	Name        string      `json:"name,omitempty"`
+	Status      FoodStatus  `json:"status,omitempty"`
+	ClaimTime   metav1.Time `json:"claimTime,omitempty"`
+	ArrivalTime metav1.Time `json:"arrivalTime,omitempty"`
+	ClaimBy     string      `json:"claimBy,omitempty"`
+}
+
+//FoodStatus .
+type FoodStatus string
+
+const (
+	//FoodStatusPending .
+	FoodStatusPending = "Pending"
+	//FoodStatusPendingArrival .
+	FoodStatusPendingArrival = "PendingArrival"
+	//FoodStatusArrived .
+	FoodStatusArrived = "Arrived"
+)
+
+//+kubebuilder:object:root=true
+
+// GoddessMoment is the Schema for the goddessmoments API
+type GoddessMoment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GoddessMomentSpec   `json:"spec,omitempty"`
+	Status GoddessMomentStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// GoddessMomentList contains a list of GoddessMoment
+type GoddessMomentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GoddessMoment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&GoddessMoment{}, &GoddessMomentList{})
+}
---
+++ k8s/crd-controller/kb/controllers/goddessmoment_controller.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	kbv1 "github.com/trainyao/playground/k8s/crd-controller/kb/api/v1"
+)
+
+var (
+	foodFocus = map[string]string{
+		"小明": "珍珠奶茶",
+		"小王": "麻辣烫",
+		"小杰": "螺蛳粉",
+	}
+	buyFoodTimeCostMap = map[string]time.Duration{
+		"珍珠奶茶": time.Second,
+		"麻辣烫":  5 * time.Second,
+		"螺蛳粉":  10 * time.Second,
+	}
+)
+
+// GoddessMomentReconciler reconciles a GoddessMoment object
+type GoddessMomentReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Name   string
+}
+
+//+kubebuilder:rbac:groups=kb.crd.playground.trainyao.io,resources=goddessmoments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=kb.crd.playground.trainyao.io,resources=goddessmoments/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=kb.crd.playground.trainyao.io,resources=goddessmoments/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the GoddessMoment object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+func (r *GoddessMomentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	res := ctrl.Result{}
+
+	var goddessMoment kbv1.GoddessMoment
+	err := r.Get(ctx, req.NamespacedName, &goddessMoment)
+	if err != nil {
+		// The GoddessMoment resource may no longer exist, in which case we stop
+		// processing.
+		if errors.IsNotFound(err) {
+			return res, fmt.Errorf("gm '%s' in work queue no longer exists", req.Name)
+		}
+
+		res.Requeue = true
+		return res, err
+	}
+
+	focusFood, ok := foodFocus[r.Name]
+	if !ok {
+		return res, fmt.Errorf("%s 朋友圈找不到 %s 的关注食物", goddessMoment.Name, focusFood)
+	}
+	logger.Info("关注食物", "name", r.Name, "food", focusFood)
+
+	foundFocusFood := false
+	for _, food := range goddessMoment.Spec.FoodDemand {
+		if food.Name == focusFood {
+			foundFocusFood = true
+			break
+		}
+	}
+
+	if !foundFocusFood {
+		logger.Info("关注食物没有在朋友圈发现", "name", r.Name, "food", focusFood, "goddessmoment", req.Name)
+		return res, nil
+	}
+
+	var foodStatus *kbv1.FoodDemandStatus
+	var index = -1
+	for i, foodDemandStatus := range goddessMoment.Status.FoodDemand {
+		if foodDemandStatus.Name == focusFood {
+			foodStatus = &goddessMoment.Status.FoodDemand[i]
+			index = i
+		}
+	}
+	if foodStatus == nil {
+		return res, fmt.Errorf("关注食物 %s 在女神朋友圈出现, 但是在status里没有找到", focusFood)
+	}
+
+	logger.Info("朋友圈关注的食物状态是", "goddessMoment", goddessMoment.Name, "name", r.Name, "food", focusFood, "status", foodStatus.Status)
+
+	if foodStatus.Status == kbv1.FoodStatusPending {
+		// 处理女神朋友圈里关注食物还未被人认领的情况
+		err = r.handlePending(ctx, &goddessMoment, index)
+	} else {
+		// 处理关注食物已经有人认领的情况
+
+		// 认领的人不是我
+		if foodStatus.ClaimBy != r.Name {
+			logger.Info("朋友圈食物已经被人认领了, 我来的太迟了", "goddessMoment", goddessMoment.Name, "food", focusFood, "name", foodStatus.ClaimBy)
+			return res, nil
+		}
+
+		if foodStatus.Status == kbv1.FoodStatusPendingArrival {
+			// 去购买食物
+			err = r.handlePendingArrival(ctx, &goddessMoment, index)
+		}
+
+		//  状态是已到达, 本条朋友圈任务已完成
+		logger.Info("朋友圈食物我已经认领并送到了, 可以不用关注了", "goddessMoment", goddessMoment.Name, "food", focusFood)
+	}
+
+	if err != nil {
+		err = fmt.Errorf("处理朋友圈 %s 更新失败, err: %s", goddessMoment.Name, err.Error())
+		res.Requeue = true
+		return res, err
+	}
+
+	return res, nil
+}
+
+func (r *GoddessMomentReconciler) handlePending(ctx context.Context, gm *kbv1.GoddessMoment, index int) error {
+	logger := log.FromContext(ctx)
+
+	focusFood := gm.Status.FoodDemand[index].Name
+	logger.Info("开始告诉女神我来送", "food", focusFood)
+	if r.Name == "小杰" {
+		// 展示patch更新
+		data := `[{"op":"replace","path":"/status/foodDemand","value":%s}]`
+		foodDemandStatusCopy := gm.Status.DeepCopy()
+		foodDemandStatusCopy.FoodDemand[index].Status = kbv1.FoodStatusPendingArrival
+		foodDemandStatusCopy.FoodDemand[index].ClaimTime = metav1.Now()
+		foodDemandStatusCopy.FoodDemand[index].ClaimBy = r.Name
+
+		valueJSON, err := json.Marshal(&foodDemandStatusCopy.FoodDemand)
+		if err != nil {
+			return fmt.Errorf("marshal foodDemandStatusCopy.FoodDemand 失败, err: %s", err.Error())
+		}
+
+		dataJSON := fmt.Sprintf(data, valueJSON)
+		err = r.Patch(ctx, gm, client.RawPatch(types.JSONPatchType, []byte(dataJSON)))
+		if err != nil {
+			return fmt.Errorf("patch更新朋友圈告诉女神 %s 我来送 %s 失败, err: %s, dataJSON: %s", gm.Name, focusFood, err.Error(), dataJSON)
+		}
+	} else {
+		// 使用update更新
+		gmCopy := gm.DeepCopy()
+		gmCopy.Status.FoodDemand[index].Status = kbv1.FoodStatusPendingArrival
+		gmCopy.Status.FoodDemand[index].ClaimTime = metav1.Now()
+		gmCopy.Status.FoodDemand[index].ClaimBy = r.Name
+
+		err := r.Update(ctx, gmCopy)
+		if err != nil {
+			return fmt.Errorf("更新朋友圈 %s 告诉女神 %s 我来送失败, err: %s", gm.Name, focusFood, err.Error())
+		}
+	}
+
+	logger.Info("更新朋友圈告诉女神送来成功", "goddessMoment", gm.Name, "food", focusFood)
+	return nil
+}
+
+func (r *GoddessMomentReconciler) handlePendingArrival(ctx context.Context, gm *kbv1.GoddessMoment, index int) error {
+	logger := log.FromContext(ctx)
+
+	focusFood := gm.Status.FoodDemand[index].Name
+	buyFoodTimeCost := buyFoodTimeCostMap[focusFood]
+	time.Sleep(buyFoodTimeCost)
+	logger.Info("为朋友圈购买需要秒, 正在前往购买", "goddessMoment", gm.Name, "food", focusFood, "buyFoodTime", buyFoodTimeCost/time.Second)
+
+	gmCopy := gm.DeepCopy()
+	gmCopy.Status.FoodDemand[index].Status = kbv1.FoodStatusArrived
+	gmCopy.Status.FoodDemand[index].ArrivalTime = metav1.Now()
+	err := r.Update(ctx, gmCopy)
+	if err != nil {
+		return fmt.Errorf("更新朋友圈 %s 告诉女神送来 %s 失败, err: %s", gm.Name, focusFood, err.Error())
+	}
+	logger.Info("更新朋友圈告诉女神送来成功", "goddessMoment", gm.Name, "food", focusFood)
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *GoddessMomentReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&kbv1.GoddessMoment{}).
+		Complete(r)
+}
